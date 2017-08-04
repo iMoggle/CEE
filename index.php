abd07160acc30e158b1aaca89e537dca44bdfbf9
Francisco Javier Montiel Morán <fmontielm@gmail.com>
2017-08-04 14:34:45 -0500

    Se agrego el index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Francisco Javier Montiel Morán
+ * Correo: francisco.montiel@enlace.edu.mx
+ * Date: 04/08/2017
+ * Time: 02:33 PM
+ */
+?>
+<html>
+<body>
+<p>Esta es una pagina de Prueba.</p>
+<a href="login.php">Ingresa a la cuenta</a>
+</body>
+</html>

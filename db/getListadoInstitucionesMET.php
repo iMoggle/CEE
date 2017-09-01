146e1a021e742ae12d85027f2d54bc8410b9d711
Francisco Javier Montiel Morán <fmontielm@gmail.com>
2017-09-01 03:05:39 -0500

    Archivo de consultas para la opcion uno de los formularios

--- db/getListadoInstitucionesMET.php
+++ db/getListadoInstitucionesMET.php
@@ -15,29 +15,13 @@ if (count($_POST) > 0) {
     $p_nivel = isset($_POST["op1_nivel"]) ? $_POST["op1_nivel"] : '';
     $p_limite = isset($_POST["op1_limite"]) ? $_POST["op1_limite"] : '0';
 
-    $db_query = "CALL listado_instituciones_met_sup('$p_ciclo', '$p_nombreClave', $p_entidad, $p_control, $p_nivel, $p_limite)";
+    $db_query = "CALL listado_instituciones_met_sup('$p_ciclo', '$p_nombreClave', '$p_entidad', '$p_control', '$p_nivel', '$p_limite')";
     $db_result = query($db_query);
 
-    $cadena = '<table class="table table-bordered">
-                                    <thead>
-                                    <tr>
-                                        <td>Institucion</td>
-                                        <td>Nombre Institucion</td>
-                                        <td>Escuela</td>
-                                        <td>Nombre Escuela</td>
-                                        <td>Carrera</td>
-                                        <td>Nombre de Carrera</td>
-                                        <td>Control</td>
-                                        <td>Sostenimiento</td>
-                                        <td>Matricula</td>
-                                        <td>Egresados</td>
-                                        <td>Titulados</td>
-                                    </tr>
-                                    </thead>
-                                    <tbody>';
+    $cadena = '';
 
-    if (num($db_query) > 0) {
-        for ($indice = 0; $indice < num($db_query); $indice++) {
+    if (num($db_result) > 0) {
+        for ($indice = 0; $indice < num($db_result); $indice++) {
             $row = arreglo($db_result);
             $fila = "<tr>";
             $fila .= "<td>" . $row["CV_INSTITUCION"] . "</td><td>" . $row["NM_INSTITUCION"] . "</td><td>" . $row["CV_ESCUELA"] . "</td>";
@@ -48,6 +32,5 @@ if (count($_POST) > 0) {
             $cadena .= $fila;
         }
     }
-    $cadena = "</tbody></table>";
     echo $cadena;
 }
---
+++ db/getListadoInstitucionesMET_xls.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MAEUSOGO
+ * Date: 01/09/2017
+ * Time: 02:25 AM
+ */
